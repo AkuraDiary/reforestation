c7a65c3f4ce3446c862db839f063af757eab40ec
Ahsan <ahsanaz461@gmail.com>
2025-03-02 14:22:10 +0700

    Add clap

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
+use clap::Parser;
 use std::env;
 use std::fs;
 use std::process::Command;
 
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
     let target_dir = "dummy-git-repo";
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name);
+    }
 
     // Create directory if it doesn't exist
     fs::create_dir_all(target_dir).expect("Failed to create directory");
